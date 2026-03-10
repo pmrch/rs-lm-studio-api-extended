b58fad956b32f3b48c921c9a2ce7951f9a20642f
pmrch <fejespatrik2007@gmail.com>
2026-03-11 00:16:10 +0100

    Created mirror workflow

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-extern crate lm_studio_api_extended;  use lm_studio_api_extended::{ Model, Context, Chat, Request };
+extern crate lm_studio_api_extended;
+use lm_studio_api_extended::{Chat, Context, Model, Request};
 
 #[tokio::main]
 async fn main() {
     // init chat:
     let mut chat = Chat::new(
-        Model::Gemma3_4b,   // select AI model
-        Context::new("You're Jarvis - my personal assistant. Call me master", 4090),  // write system prompt + set max size of messages context
+        Model::Llama3_1_8b, // select AI model
+        Context::new(
+            "You're Jarvis - my personal assistant. Call me master",
+            4090,
+        ), // write system prompt + set max size of messages context
     );
-    
 
     // NO STREAM TEST:
-    
     // init request:
     let request = Request {
         messages: vec!["Hi, what's your name?".into()],
@@ -24,7 +26,7 @@ async fn main() {
 
     match result {
         // print results part:
-        Ok(Some(response)) =>  println!("{}", response.text()),
+        Ok(Some(response)) => println!("{}", response.text()),
 
         // print error:
         Err(e) => eprintln!("Error: {e}"),
@@ -32,19 +34,18 @@ async fn main() {
         _ => {}
     }
 
-
     // STREAM TEST:
 
     loop {
         // reading user input:
         eprint!("\n>> ");
-        
+
         let mut buf = String::new();
         std::io::stdin().read_line(&mut buf).unwrap();
 
         // generating answer:
         eprint!("<< ");
-        
+
         // init request:
         let request = Request {
             messages: vec![buf.into()],
@@ -52,7 +53,7 @@ async fn main() {
             stream: true,
             ..Request::default()
         };
-        
+
         // sending request:
         let _ = chat.send(request).await.unwrap();
 
@@ -62,13 +63,13 @@ async fn main() {
                 // print results part:
                 Ok(text) if !text.is_empty() => {
                     eprint!("{text}");
-                },
+                }
 
                 // print error:
                 Err(e) => {
                     eprintln!("Error: {e}");
                     break;
-                },
+                }
 
                 _ => {}
             }
